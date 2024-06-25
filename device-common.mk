c2ea09dfdf4a704b1a8df9edcba7f5f3c50f8b5b
eduyl <proman312008@gmail.com>
2024-06-25 15:15:55 +0530

    reverting some changes

--- device-common.mk
+++ device-common.mk
@@ -261,9 +261,7 @@ $(call inherit-product, frameworks/native/build/phone-xhdpi-4096-dalvik-heap.mk)
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH) \
-    hardware/google/interfaces \
-    hardware/google/pixel \
-    hardware/samsung
+
 
 # System
 PRODUCT_PROPERTY_OVERRIDES += \
